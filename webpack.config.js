c922ba1833b1827b98addb78c425802cefa60282
Zbigi Man Zbigniew Stępniewski <zbigiman@gmail.com>
2019-09-09 20:44:38 +0200

    build

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,22 @@ const STYLES_ENTRY = './src/scss/styles.scss'
 const STYLES_OUTPUT = './styles'
 const STYLES_FILENAME = 'styles.css'
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const extractSASS = new ExtractTextPlugin(STYLES_FILENAME)
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const miniCssExtractPlugin = new MiniCssExtractPlugin({
+  filename: STYLES_FILENAME
+})
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const optimizeCSSAssetsPlugin = new OptimizeCSSAssetsPlugin({
+  cssProcessorOptions: {
+    cssProcessor: require('cssnano'),
+    cssProcessorPluginOptions: {
+      preset: ['default', { discardComments: { removeAll: true } }]
+    },
+    canPrint: true
+  }
+})
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = env => {
   const MODE = env && env.prod ? 'production' : 'development'
@@ -44,37 +58,51 @@ module.exports = env => {
     },
     plugins: [
       definePlugin
-    ]
+    ],
+    performance: { hints: false }
   }, {
     mode: MODE,
     entry: STYLES_ENTRY,
     output: {
-      path: path.resolve(__dirname, STYLES_OUTPUT),
-      filename: STYLES_FILENAME
+      path: path.resolve(__dirname, STYLES_OUTPUT)
     },
     module: {
-      rules: [{
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader',
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
             {
-              loader: 'sass-loader',
+              loader: MiniCssExtractPlugin.loader,
               options: {
-                includePaths: [
-                  path.resolve('node_modules/semantic-ui-sass/')
-                ]
+                hmr: process.env.NODE_ENV === 'development'
               }
-            }
+            },
+            'css-loader',
+            'sass-loader'
           ]
-        })
-      }, {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
-      }]
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+          loader: 'url-loader?limit=100000'
+        }]
     },
     plugins: [
-      extractSASS
-    ]
+      miniCssExtractPlugin,
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: [
+          path.resolve(__dirname, STYLES_OUTPUT + '/*.*')
+        ],
+        cleanAfterEveryBuildPatterns: [
+          path.resolve(__dirname, STYLES_OUTPUT + '/main.js')
+        ]
+      })
+    ],
+    optimization: {
+      minimizer: [
+        optimizeCSSAssetsPlugin
+      ]
+    },
+    performance: { hints: false }
   }]
 }
